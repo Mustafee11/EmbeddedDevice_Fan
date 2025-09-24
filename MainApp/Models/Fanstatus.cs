3a8ee5c13e91637fc1955c837f76cdf2073ff46a
Mustafee <mussemoh03@gmail.com>
2025-09-24 22:56:09 +0200

    finished models.

---
+++ MainApp/Models/Fanstatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp.Models
+{
+    class Fanstatus
+    {
+        public string Id { get; set; } =Guid.NewGuid().ToString();
+        public bool IsRunning { get; set; } = false;
+
+        public double Speed { get; set; }
+
+
+    }
+}
